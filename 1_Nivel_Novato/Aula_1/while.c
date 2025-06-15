fea4e7871f8c7593b89a4304289c8246d768a201
Italo Santos <italotsantana75@gmail.com>
2025-06-15 15:34:05 -0300

    Início Nível Novato e Prática Aula 1

---
+++ 1_Nivel_Novato/Aula_1/while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int i = 0;
+
+
+
+    while (i < 10) {
+        printf("i = %d\n", i);
+        i++; 
+    }
+
+
+
+    printf("Valor final de i = %d\n", i);
+
+
+
+    return 0;
+}
